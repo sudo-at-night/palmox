dfdf47ba57b50e483607b43e61fb7032775f0be2
Patryk Mazur <sudoatnight@gmail.com>
2021-05-10 23:44:07 +0200

    Test api calls + ignore mock server and config TS files

---
+++ src/frontend/src/api/calls.test.ts
@@ -0,0 +1,23 @@
+jest.mock('api/client', () => ({
+    client: {
+        call: jest.fn(),
+    },
+}))
+
+import { client } from 'api/client'
+import * as CALLS from './calls'
+
+const call = <jest.Mock>client.call
+
+describe('API Calls', () => {
+    test('Log In -> Uses email and password to call the API with JSON string', () => {
+        const credentials = { email: 'username@mail.com', password: 'secret' }
+
+        CALLS.callLogIn(credentials)
+
+        const callArgs = call.mock.calls[0][1]
+        const callBody = JSON.parse(callArgs.body)
+
+        expect(callBody).toEqual(callBody)
+    })
+})
